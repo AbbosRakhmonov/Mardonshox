d4725af8019f336099973edec909eacf20cbbe4a
Rakhomonov <abbos.raxmonov.2001@gmail.com>
2022-10-16 13:07:35 +0500

    base url changed

--- src/Config/api.js
+++ src/Config/api.js
@@ -2,8 +2,10 @@ import axios from 'axios'
 import {logOut} from '../Features/Auth/authSlice'
 import Store from '../App/store'
 
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api/v1' : 'https://mardonshox.herokuapp.com/api/v1'
+
 const instance = axios.create({
-    baseURL: `http://localhost:5000/api/v1`,
+    baseURL: baseURL,
     headers: {
         'Content-Type': 'application/json'
     }
