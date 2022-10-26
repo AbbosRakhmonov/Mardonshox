c8c9761c9a96ec891104c6112be8cbeb99a0f139
ShokhruhKholboyev <shoxruh9902@gmail.com>
2022-10-26 10:29:43 +0500

    shoxni kompidan tugirladim

--- src/Features/protectedRoutes.js
+++ src/Features/protectedRoutes.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import {Navigate, Outlet} from 'react-router-dom'
-import {useSelector, useDispatch} from 'react-redux'
-import {logOut} from './auth/authSlice'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logOut } from "./Auth/authSlice"
 
 function ProtectedRoutes() {
     const dispatch = useDispatch()
-    const {isLogged} = useSelector(state => state.auth)
+    const { isLogged } = useSelector(state => state.auth)
     if (!isLogged) {
         dispatch(logOut("Autorizatsiyadan o`tilmagan"))
-        return <Navigate to={'/login'} replace={true}/>
+        return <Navigate to={'/login'} replace={true} />
     }
-    return <Outlet/>
+    return <Outlet />
 }
 
 export default ProtectedRoutes
