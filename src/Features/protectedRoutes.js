bd04de124e32117f37d7c3625a41595582b24945
Abbosbek <abbos.raxmonov.2001@gmail.com>
2022-10-26 10:08:02 +0500

    Update protectedRoutes.js

--- src/Features/protectedRoutes.js
+++ src/Features/protectedRoutes.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import {Navigate, Outlet} from 'react-router-dom'
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
+import {logOut} from './auth/authSlice'
 
 function ProtectedRoutes() {
+    const dispatch = useDispatch()
     const {isLogged} = useSelector(state => state.auth)
     if (!isLogged) {
+        dispatch(logOut("Autorizatsiyadan o`tilmagan"))
         return <Navigate to={'/login'} replace={true}/>
     }
     return <Outlet/>
