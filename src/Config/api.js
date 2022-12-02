cae8d8f1f471149b6a396e0b80b0d645765a0bc1
Abbosbek <abbos.raxmonov.2001@gmail.com>
2022-12-02 18:20:03 +0500

    Update api.js

--- src/Config/api.js
+++ src/Config/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { logOut } from '../Features/Auth/authSlice'
 import Store from '../App/store'
 
-const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api/v1' : 'https://mardonshox.herokuapp.com/api/v1'
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api/v1' : 'https://mardonshox.onrender.com/api/v1'
 
 const instance = axios.create({
     baseURL: baseURL,
